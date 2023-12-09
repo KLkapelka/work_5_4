b32e05fe750d93cddcefb11577df8cb84f43bed0
KLkapelka <153184633+KLkapelka@users.noreply.github.com>
2023-12-09 11:30:32 +0700

    Create Program.cs

---
+++ project/Program.cs
@@ -0,0 +1,24 @@
+﻿// Дан файл numsTask4.txt с целыми числами. Вычислите сумму элементов, отличающихся от максимального на 1;
+
+using System;
+using System.IO;
+using System.Linq;
+class work_5_4
+{
+    static void Main()
+    {
+        string[] use = File.ReadAllLines("numsTask4.txt"); 
+        int[] chosenUse = use[0].Split(' ').Select(int.Parse).ToArray(); 
+        int max = chosenUse.Max(); 
+        int sum = 0; 
+
+        for (int i = 0; i < chosenUse.Length; i++) 
+        {
+            if (chosenUse[i] == max - 1) 
+            {
+                sum += chosenUse[i]; 
+            }
+        }
+        Console.WriteLine($"Произведение элементов после минимального числа: {sum}");
+    }
+}
